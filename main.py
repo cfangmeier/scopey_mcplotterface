16c6683e4f2772c0addf24db78e239722c9dcc29
Caleb Fangmeier <caleb@fangmeier.tech>
2020-03-16 17:13:09 -0500

    Adds comparison of pulse heights with and without a source

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotboard as mpb
 from functools import lru_cache
+import mplhep as hep
 
 
 @lru_cache()
@@ -12,9 +13,11 @@ def get_trigger(folder, number, channel):
     return lcr.read(filename)
 
 
-def triggers_in_folder(folder, channel):
+def triggers_in_folder(folder, channel, limit=None):
     i = 1
     while i := i + 1:
+        if limit and i > limit:
+            break
         try:
             yield get_trigger(folder, i, channel)
         except FileNotFoundError:
@@ -27,9 +30,11 @@ def get_trace(folder, number, channel):
     return times, samples
 
 
-def traces_in_folder(folder, channel):
+def traces_in_folder(folder, channel, limit=None):
     i = 0
     while i := i + 1:
+        if limit and i > limit:
+            break
         try:
             yield get_trace(folder, i, channel)
         except FileNotFoundError:
@@ -37,12 +42,8 @@ def traces_in_folder(folder, channel):
 
 
 def duration_of_experiment(folder, channel):
-    # Something is wrong with how the trigger_time is being saved/read from the .trc files
-    # All times are the same within microseconds
-    # Will need to investigate further.
     import datetime
     all_times = [metadata['trigger_time'] for metadata, _, _ in triggers_in_folder(folder, channel)]
-    print(all_times)
 
     def to_dt(a):
         secs, usecs = np.divmod(a[5], 1.0)
@@ -62,9 +63,36 @@ def pulse_height_histogram(folder, title, channel):
     for data in traces_in_folder(folder, channel):
         _, sample_max = find_pulse_height(*data)
         phs.append(sample_max)
-    plt.hist(phs, bins=20)
+    h, bins = np.histogram(phs, bins=np.linspace(1.0, 3.0, 15))
+    duration = duration_of_experiment(folder, channel)
+    hep.histplot(h/duration, bins, yerr=np.sqrt(h)/duration, edges=True)
     plt.title(title)
 
+
+@mpb.decl_fig
+def pulse_height_comparison(dataset, title, channel):
+    def get_phs(sub_dataset):
+        folder = f'trace_data/{dataset}/{sub_dataset}'
+        phs = [find_pulse_height(*data)[1] for data in traces_in_folder(folder, channel)]
+        duration = duration_of_experiment(folder, channel)
+        return phs, duration
+
+    nosrc_phs, nosrc_duration = get_phs('nosrc')
+    h, bins = np.histogram(nosrc_phs, bins=np.linspace(1.0, 3.0, 15))
+    hep.histplot(h/nosrc_duration, bins, yerr=np.sqrt(h)/nosrc_duration, edges=True, label='Background')
+
+    withsrc_phs, withsrc_duration = get_phs('withsrc')
+    h, bins = np.histogram(withsrc_phs, bins=np.linspace(1.0, 3.0, 15))
+    hep.histplot(h/withsrc_duration, bins, yerr=np.sqrt(h)/withsrc_duration, edges=True, label='Background+Signal')
+
+    plt.title(title)
+    plt.legend()
+
+    return f'''
+# Data Collected on {dataset}
+  - *No Source*: {len(nosrc_phs)} Triggers over {nosrc_duration/3600:.2f} Hours
+  - *With 1 uCi Cs-137 Source*: {len(withsrc_phs)} Triggers over {withsrc_duration/3600:.2f} Hours
+'''
 
 @mpb.decl_fig
 def intensity_plot(folder, title, channel):
@@ -78,14 +106,15 @@ def intensity_plot(folder, title, channel):
 
 
 if __name__ == '__main__':
-    nosrc = 'trace_data/2020_03_06/nosrc/'
-    withsrc = 'trace_data/2020_03_06/withsrc/'
-
+    dataset = '2020_03_13'
+    nosrc = f'trace_data/{dataset}/nosrc/'
+    withsrc = f'trace_data/{dataset}/withsrc/'
     plots = {
-        'nosource': intensity_plot(nosrc, 'No Source', 'C4'),
-        'withsource': intensity_plot(withsrc, 'With Source', 'C4'),
-        'nosource-ph': pulse_height_histogram(nosrc, 'No Source', 'C4'),
-        'withsource-ph': pulse_height_histogram(withsrc, 'With Source', 'C4'),
+        # 'nosource': intensity_plot(nosrc, 'No Source', 'C4'),
+        # 'withsource': intensity_plot(withsrc, 'With Source', 'C4'),
+        # 'nosource-ph': pulse_height_histogram(nosrc, 'No Source', 'C4'),
+        # 'withsource-ph': pulse_height_histogram(withsrc, 'With Source', 'C4'),
+        'comparison-ph': pulse_height_comparison(dataset, 'Comparison', 'C4'),
     }
     mpb.render(plots)
     mpb.generate_report(plots, 'Scintillator Traces')
